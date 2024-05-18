ce72b0f14977a9fddd66cb050024c85cc65634eb
NASUNJIN <sj010618@gmail.com>
2024-05-18 19:57:54 +0900

    UPDATE validate

--- routes/channels.js
+++ routes/channels.js
@@ -1,118 +1,173 @@
-const express = require('express')
-const router = express.Router()
-router.use(express.json())
-const conn = require('../mariadb')
+const express = require('express');
+const router = express.Router();
+const conn = require('../mariadb');
+const { body, param, validationResult } = require('express-validator');
 
-let db = new Map()
-var id = 1
+router.use(express.json())
+
+// middleware
+// validate가 하나의 모듈이 됨
+const validate = (req, res, next) => {
+    const err = validationResult(req)
+
+    if (err.isEmpty()) {  // err가 없을 경우 (에러값이 비었을 경우)
+        return next(); // 다음 할 일 찾아가삼 (미들웨어, 함수)
+    } else {
+        console.log(err.array())
+        return res.status(400).json(err.array())
+    }
+}
+
+
 
 // route 사용
 router
     .route('/')
     // 채널 전체 조회
-    .get((req, res) => {
-        var { userId } = req.body  // userId 받아오기
+    .get(
+        // 유효성 검사 : 사용자의 요청에 대한 내용
+        [
+            body('userId').notEmpty().isInt().withMessage('숫자만 입력해주세요'),
+            validate
+        ],
+        (req, res, next) => {
 
-        let sql = `SELECT * FROM channels WHERE user_id = ?`
-        // userId가 있을 경우
-        if (userId) { 
+            var { userId } = req.body  // userId 받아오기
+
+            let sql = `SELECT * FROM channels WHERE user_id = ?`
             conn.query(sql, userId,
-                function (err, results, fields) {
+                function (err, results) {
+                    if(err) {   // 에러 발생
+                        console.log(err)
+                        return res.status(400).end
+                    }
+
                     if (results.length) {  // 결과값이 있을 경우
                         res.status(200).json(results)
                     } else {
-                       notFoundChannel(res)
+                        res.status(404).json({
+                            message : "채널 정보를 찾을 수 없습니다."
+                        })
                     }
                 }
-            )
-        } else {  // userId에 값이 없을 경우
-            res.status(400).end()
-        }
+            ) 
     })
 
-    // 채널 개별 생성 = db 저장
-    .post((req, res) => {
-        const { name, userId, } = req.body  // userId : 변수
-        if (name && userId) {  // name, userId 모두 있을 경우
-
+    // 채널 개별 생성
+    .post(
+        // validator express로 받아온 body, 비어선 안되고, 정수여야함
+        [
+            body('userId').notEmpty().isInt().withMessage('숫자만 입력해주세요'),  // userId 유효성 검사
+            body('name').notEmpty().isString().withMessage('문자만 입력해주세요'),
+            validate
+        ]     // name 유효성 검사
+        , (req, res) => {
+            const { name, userId } = req.body
+        
             let sql = `INSERT INTO channels (name, user_id) VALUES (?, ?)`  // user_id : db coulmn 이름
             let values = [name, userId]
             conn.query(sql, values,
                 function (err, results) {
+                    if (err) {
+                        console.log(err)
+                        return res.status(400).end()
+                    }
                     res.status(201).json(results)
                 }
             )
-        } else {
-            res.status(400).json({
-                message : "요청 값을 제대로 보내주세요."
-            })
-        }
     })
-
-
 
 router
     .route('/:id')
     // 채널 개별 조회
-    .get((req, res) => {
-        let {id} = req.params
-        id = parseInt(id)
+    .get(
+        [
+            param('id').notEmpty().withMessage('채널 id 필요'),
+            validate
+        ],
+        (req, res) => {
+            let {id} = req.params
+            id = parseInt(id)
 
-        let sql = `SELECT * FROM channels WHERE id = ?`
-        conn.query(sql, id,
-            function (err, results, fields) {
-                if (results.length) {  // 결과값이 있을 경우
-                    res.status(200).json(results)
-                } else {
-                   notFoundChannel(res)
+            let sql = `SELECT * FROM channels WHERE id = ?`
+            conn.query(sql, id,
+                function (err, results, fields) {
+                    if (err) {   // err 발생
+                        console.log(err)
+                        return res.status(400).end()
+                    }
+
+                    if (results.length) {  // 결과값이 있을 경우
+                        res.status(200).json(results)
+                    } else {
+                        res.status(404).json({
+                            message : "채널 정보를 찾을 수 없습니다."
+                        })
+                    }
                 }
-            }
-        );
+            )
     })
 
     // 채널 개별 수정
-    .put((req, res) => {
-        let {id} = req.params
-        id = parseInt(id)
+    .put(
+        [
+            param('id').notEmpty().withMessage('채널 id 필요'),
+            body('name').notEmpty().isString().withMessage('채널명 오류'),
+            validate
+        ],
+        (req, res) => {
+            let { id } = req.params
+            id = parseInt(id)
+            let { name } = req.body
 
-        var channel = db.get(id)
-        var oldTitle = channel.channelTitle
-        if (channel) {  // 객체 있음
-            var newTitle = req.body.channelTitle
+            // channel의 name을 바꿔달라 id값이 ?을
+            let sql = `UPDATE channels SET name = ? WHERE id = ?`
+            let values = [name, id]
+            conn.query(sql, values,
+                function (err, results, fields) {
+                    if (err) {   // err 발생
+                        console.log(err)
+                        return res.status(400).end()
+                    }
 
-            channel.channelTitle = newTitle
-            db.set(id, channel)
-
-            res.json({
-                message : `채널명이 ${oldTitle}에서 ${newTitle}로 수정되었습니다.`
-            })
-        } else {
-            notFoundChannel()
-        }
+                    // 없는 id 값을 수정 할 경우
+                    if (results.affectedRows == 0) {
+                        console.log(err)
+                        return res.status(400).end()
+                    } else {
+                        res.status(200).json(results)
+                    }
+                }
+            )
     })
 
     // 채널 개별 삭제
-    .delete((req, res) => {
-        let {id} = req.params
-        id = parseInt(id)
+    .delete(
+        [
+            param('id').notEmpty().isInt().withMessage('채널 id 필요'),
+            validate
+        ],
+        (req, res) => {
+            let {id} = req.params
+            id = parseInt(id)
 
-        var channel = db.get(id)
-        if (channel) {  // 객체 있음
-            db.delete(id)
+            let sql = `DELETE FROM channels WHERE id = ?`
+            conn.query(sql, id,
+                function (err, results, fields) {
+                    if (err) {   // err 발생
+                        console.log(err)
+                        return res.status(400).end()
+                    }
 
-            res.status(200).json({
-                message : `${channel.channelTitle}이 정상적으로 삭제되었습니다.`
-            })
-        } else {
-            notFoundChannel()
-        }
+                    // 없는 id 값을 수정 할 경우
+                    if (results.affectedRows == 0) {
+                        console.log(err)
+                        return res.status(400).end()
+                    } else {
+                        res.status(200).json(results)
+                    }
+                }
+            )
     })
-
-// 채널 정보를 찾을 수 없습니다.
-function notFoundChannel(res) {
-    res.status(404).json({
-        message : "채널 정보를 찾을 수 없습니다."
-    })
-}
 
 module.exports = router  // 모듈화
--- routes/users.js
+++ routes/users.js
@@ -2,83 +2,142 @@
 const express = require('express')
 const router = express.Router() // app.js가 user-demo를 찾을 수 있게 해줌 > express의 router로 사용가능
 const conn = require('../mariadb')
+const { body, param, validationResult } = require('express-validator'); // 유효성 검사
 
 router.use(express.json())  // http 외 모듈 'json'
 
+// middleware
+// validate가 하나의 모듈이 됨
+const validate = (req, res, next) => {
+    const err = validationResult(req)
+
+    if (err.isEmpty()) {  // err가 없을 경우 (에러값이 비었을 경우)
+        return next(); // 다음 할 일 찾아가삼 (미들웨어, 함수)
+    } else {
+        console.log(err.array())
+        return res.status(400).json(err.array())
+    }
+}
+
 // 로그인
 router
-    .post('/login', (req, res) => {
-        const { email, password } = req.body
-        
-        let sql = `SELECT * FROM users WHERE email = ?`
-        conn.query (sql, email,  // ?에 email 값이 들어옴
-            function (err, results) {
-                var loginUser = results[0];
-                // 로그인 유저가 있을 경우 && results로 받아온 password의 값이 body 값의 (db) password랑 같다면
-                if (loginUser && (loginUser.password == password)) {  
-                    res.status(200).json({
-                        message : `${loginUser.name}님 로그인 되었습니다.`
-                    })
-                } else {  // 회원이 없을 경우
-                    res.status(404).json({
-                        message : `이메일 또는 비밀번호가 틀렸습니다.`
-                    })
+    .post(
+        '/login', 
+        [
+            body('email').notEmpty().isEmail().withMessage('이메일 확인 필요'),
+            body('password').notEmpty().isString().withMessage('비밀번호 확인 필요'),
+            validate
+        ],
+        (req, res) => {
+            const { email, password } = req.body
+            
+            let sql = `SELECT * FROM users WHERE email = ?`
+            conn.query (sql, email,  // ?에 email 값이 들어옴
+                function (err, results) {
+                    if (err) {   // err 발생
+                        console.log(err)
+                        return res.status(400).end()
+                    }
+                    
+                    var loginUser = results[0];
+                    // 로그인 유저가 있을 경우 && results로 받아온 password의 값이 body 값의 (db) password랑 같다면
+                    if (loginUser && (loginUser.password == password)) {  
+                        res.status(200).json({
+                            message : `${loginUser.name}님 로그인 되었습니다.`
+                        })
+                    } else {  // 회원이 없을 경우
+                        res.status(404).json({
+                            message : `이메일 또는 비밀번호가 틀렸습니다.`
+                        })
+                    }
                 }
-            }
-        );
+            );
     })
 
 // 회원가입
 router
-    .post('/join', (req, res) => {
-        // body에 값이 있을 경우
-        if (req.body == {}) {
-            res.status(404).json({
-                message : `입력 값을 다시 확인해주세요.`
-            })
-        } else {  // body에 값이 없을 경우
+    .post(
+        '/join', 
+        [
+            body('email').notEmpty().isEmail().withMessage('이메일 확인 필요'),
+            body('name').notEmpty().isString().withMessage('이름 확인 필요'),
+            body('password').notEmpty().isString().withMessage('비밀번호 확인 필요'),
+            body('contact').notEmpty().isString().withMessage('연락처 확인 필요'),
+            validate
+        ],
+        (req, res) => {
             const { email, name, password, contact } = req.body
 
             let sql = `INSERT INTO users (email, name, password, contact) VALUES (?, ?, ?, ?)`
             let values = [email, name, password, contact]
             conn.query(sql, values,
                 function (err, results) {
+                    if (err) {   // err 발생
+                        console.log(err)
+                        return res.status(400).end()
+                    }
+
                     res.status(201).json(results)
                 }
             );
-        }
     })
 
 router
     // 회원 개별 조회
     .route('/users')
-    .get(function (req, res) {
-        let { email } = req.body
+    .get(
+        [
+            body('email').notEmpty().isEmail().withMessage('이메일 확인 필요'),
+            validate
+        ],
+        function (req, res) {
+            let { email } = req.body
 
-        let sql = `SELECT * FROM users WHERE email = ?`
-        conn.query(sql, email,
-            function (err, results, fields) {
-                if (results.length) {
-                    res.status(200).json(results)
-                } else {
-                    // 결과값이 없을 경우
-                    res.status(404).json({
-                        message : "회원정보가 없습니다."
-                    })
+            let sql = `SELECT * FROM users WHERE email = ?`
+            conn.query(sql, email,
+                function (err, results, fields) {
+                    if (err) {   // err 발생
+                        console.log(err)
+                        return res.status(400).end()
+                    }
+
+                    if (results.length) {
+                        res.status(200).json(results)
+                    } else {
+                        // 결과값이 없을 경우
+                        res.status(404).json({
+                            message : "회원정보가 없습니다."
+                        })
+                    }
                 }
-            }
-        );
+            );
     })
     // 회원 개별 탈퇴
-    .delete(function (req, res) {
-        let { email } = req.body
+    .delete(
+        [
+            body('email').notEmpty().isEmail().withMessage('이메일 확인 필요'),
+            validate
+        ],
+        function (req, res) {
+            let { email } = req.body
 
-        let sql = `DELETE FROM users WHERE email = ?`
-        conn.query(sql, email,
-            function (err, results, fields) {
-                res.status(200).json(results)
-            }
-        );
+            let sql = `DELETE FROM users WHERE email = ?`
+            conn.query(sql, email,
+                function (err, results, fields) {
+                    if (err) {   // err 발생
+                        console.log(err)
+                        return res.status(400).end()
+                    }
+
+                    // 없는 id 값을 수정 할 경우
+                    if (results.affectedRows == 0) {
+                        console.log(err)
+                        return res.status(400).end()
+                    } else {
+                        res.status(200).json(results)
+                    }
+                }
+            );
     })
 
 module.exports = router  // 모듈화
